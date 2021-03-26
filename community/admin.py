9c0613c7a03294bb887752ccf47079ed1c5271fb
soc4585 <soc4585@gmail.com>
2021-03-26 14:34:59 +0900

    D 완료 (Admin)

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review, Comment
+
 
 # Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'movie_title', 'rank', 'content', 'created_at', 'updated_at', 'user_id')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'content', 'review_id', 'user_id')
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
